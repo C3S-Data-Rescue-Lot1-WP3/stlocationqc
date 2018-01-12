c2c21eff415d5d2ec9788ec9f99925c1606e5ca3
Clara Ventura <mcmventura@fc.ul.pt>
2018-01-12 15:40:16 +0000

    Updating DESCRIPTION to add links to GitHub site

--- R/get_country.R
+++ R/get_country.R
@@ -1,42 +1,78 @@
 #'
-#' Determines the country names for points in the continent
+#' Determines the country names for coordinate points
 #'
-library(sp)
-library(rgdal)
-
+#' Given a pair of geographic coordinates in the continent, determines the
+#' country name.
+#'
+#' @details Input: A text file with the coordinates in decimal degrees and a
+#'   shapefile with the country names.
+#'
+#'   The .txt file has to be in the following format: with the longitude in the
+#'   first column and the latitude in the second column, separated by tabs.The
+#'   header of the file must be 'longitude' and 'latitude', respectively and
+#'   longitude must be in the range [-180, +180].
+#'
+#'   If exists <coords2getf.txt> created by \code{\link{get_lon180}},
+#'   automatically loads and reads the coordinates file. If wasn't necessary to
+#'   use \code{\link{get_lon180}}, because the longitude was already in the
+#'   range [-180, +180], opens a window, for choosing the input file that must
+#'   be in the working directory.
+#'
+#'   Creates an 'id' with the row number, which consists of an identifier to the
+#'   coordinates: the 'id' is necessary to display the coordinates in the
+#'   initial order at the end of the process.
+#'
+#'   Output: writes the country names in <countries.txt>; writes the missing
+#'   name points (if they exist) in <countries_na.txt>, which is the input of
+#'   \code{\link{get_marine}}; writes a processing report in
+#'   <report_get_country.txt>.
+#'
+#' @seealso Requires \code{\link[rgdal]{readOGR}},
+#'   \code{\link[sp]{SpatialPoints}} and \code{\link[sp]{over}} to determine the
+#'   country names.
+#'
+#' @references 1:10m Cultural Vectors: Admin 0 - Countries. Made with Natural
+#'   Earth. Free vector and raster map data @@ naturalearthdata.com. Consulted
+#'   on 2017-11-15.
+#'
+#' @examples
+#' E.g. the Portuguese territory
+#' If the point is in Continental Portugal
+#' attributes the name 'Portugal'
+#' If the point is in the Portuguese
+#' islands Azores or Madeira
+#' which are the two Autonomous Regions
+#' of the Portuguese sovereign
+#' attributes 'Azores' and 'Madeira'
+#' respectively.
+#'
+#' @export
 get_country <- function() {
-  if (file.exists("coords2getf.txt")) {
-    coords <- read.table("coords2getf.txt", header = TRUE, sep = "\t", quote = "")
-  } else {
-    print("Load the .txt file.")
-    print("Note that this function was tested for about 84.000 (maximum) pairs of coordinates.")
-    print("Loading larger files is likely to cause long processing time or memory problems.")
-    # Load and read the file
-    coords <- read.table(file.choose(), header = TRUE, sep = "\t", quote = "")
-  }
-  # Create a vector with the row number, which consists of an identifier (id) to the coordinates
-  # The "id" is required to display the coordinates in the initial order at the end of the process
-  coords_id <- row.names.data.frame(coords)
-  # Read the shapefile with the country names
-  gname_sp <- rgdal::readOGR(dsn = ".", layer = "ne_10m_admin_0_countries")
-  # SpatialPoints() receive a data.frame with only two columns (latitude and longitude)
-  # Longitude must be in the range [-180, +180]
-  # The input Coordinate Reference System (CRS) is the shapefile CRS
-  coords_sp <- sp::SpatialPoints(coords, proj4string = sp::CRS(proj4string(gname_sp))) 
-  indices <- sp::over(coords_sp, gname_sp)
-  gname <- data.frame(id = coords_id, longitude = coords$longitude, latitude = coords$latitude, country = indices$ADMIN)
-  countries <- na.omit(gname)
-  write.table(countries, file = "countries.txt", row.names = FALSE, col.names = TRUE, sep = "\t", quote = FALSE)
-  countries_na <- gname[is.na(gname$country), ]
-  # Write the missing name points in a file, which is the input of get_marine()
-  if (nrow(countries_na)!=0) {
-    write.table(countries_na, file = "countries_na.txt", row.names = FALSE, col.names = TRUE, sep = "\t", quote = FALSE)
-  }
-  report <- cbind(c("Coordinate pairs processed: ", "Country names returned: ", "Country names missing: "),                        c(nrow(gname), nrow(countries), nrow(countries_na)))
-  write.table(report, file = "report_get_country.txt", row.names = FALSE, col.names = FALSE, sep = "\t", quote = FALSE )
-  print("", quote = FALSE)
-  print("Check < countries.txt > and < report_get_country.txt >.", quote = FALSE)
-  print("If there are country names missing, run < get_marine() >.", quote = FALSE)
-  print("", quote = FALSE)
-  return(report)
+    if (file.exists("coords2getf.txt")) {
+        coords <- read.table("coords2getf.txt", header = TRUE, sep = "\t", quote = "")
+    } else {
+        print("Load the .txt file.")
+        print("Note that this function was tested for about 84.000 (maximum) pairs of coordinates.")
+        print("Loading larger files is likely to cause long processing time or memory problems.")
+        coords <- read.table(file.choose(), header = TRUE, sep = "\t", quote = "")
+    }
+    coords_id <- row.names.data.frame(coords)
+    gname_sp <- rgdal::readOGR(dsn = ".", layer = "ne_10m_admin_0_countries")
+    coords_sp <- sp::SpatialPoints(coords, proj4string = sp::CRS(proj4string(gname_sp)))
+    indices <- sp::over(coords_sp, gname_sp)
+    gname <- data.frame(id = coords_id, longitude = coords$longitude, latitude = coords$latitude, country = indices$ADMIN)
+    countries <- na.omit(gname)
+    write.table(countries, file = "countries.txt", row.names = FALSE, col.names = TRUE, sep = "\t", quote = FALSE)
+    countries_na <- gname[is.na(gname$country), ]
+    if (nrow(countries_na) != 0) {
+        write.table(countries_na, file = "countries_na.txt", row.names = FALSE, col.names = TRUE, sep = "\t", quote = FALSE)
+    }
+    report <- cbind(c("Coordinate pairs processed: ", "Country names returned: ", "Country names missing: "), c(nrow(gname),
+        nrow(countries), nrow(countries_na)))
+    write.table(report, file = "report_get_country.txt", row.names = FALSE, col.names = FALSE, sep = "\t", quote = FALSE)
+    print("", quote = FALSE)
+    print("Check < countries.txt > and < report_get_country.txt >.", quote = FALSE)
+    print("If there are country names missing, run < get_marine() >.", quote = FALSE)
+    print("", quote = FALSE)
+    return(report)
 }
--- R/get_lon180.R
+++ R/get_lon180.R
@@ -1,75 +1,82 @@
 #'
 #' Transforms the longitude from [0, 360] to [-180, +180]
-#' 
+#'
+#' Given a pair of geographic coordinates, transforms the longitude from [0,
+#' 360] to [-180, +180].
+#'
+#' Input: a .txt file with the longitude in the first column and the latitude in
+#' the second column, separated by tabs. The header of the file must be
+#' 'longitude' and 'latitude', respectively.
+#'
+#' Opens a window, for choosing the input file that must be in the working
+#' directory. Verify if the longitude and latitude are valid. Transforms the
+#' longitude.
+#'
+#' Output: creates a .txt file <coords2getf.txt> with the transformed longitude
+#' and the latitude. This file is the input of \code{\link{get_country}} or
+#' \code{\link{get_marine}} to determine the geographic names.
+#'
 get_lon180 <- function() {
-  print("Load the coordinates file.")
-  print("", quote = FALSE)
-  # Load and read the file
-  coords <- read.table(file.choose(), header = TRUE, sep = "\t", quote = "")
-  
-  print("Checking the data...", quote = FALSE)
-  print("", quote = FALSE)
-  print(str(coords))
-  print("", quote = FALSE)
-  if (nrow(coords) >= 10) {
-    print("First 10 rows of the file:", quote = FALSE)
-    print(head(coords, n = 10L))
+    print("Load the coordinates file.")
     print("", quote = FALSE)
-    print("Last 10 rows of the file:", quote = FALSE)
-    print(tail(coords, n = 10L))
+    coords <- read.table(file.choose(), header = TRUE, sep = "\t", quote = "")
+    print("Checking the data...", quote = FALSE)
     print("", quote = FALSE)
-  }
-  # Verify if the longitude and latitude are valid
-  x <- coords$longitude
-  y <- coords$latitude
-  for (i in 1:nrow(coords)) {
-    if (x[i] < 0 || x[i] > 360) {
-    print("Found an invalid longitude value!", quote = FALSE)
-    print("Row number:", quote = FALSE)
-    print(i+1)
-    print("Value:", quote = FALSE)
-    print(x[i])
+    print(str(coords))
     print("", quote = FALSE)
-    return("Please correct the invalid value(s).")
+    if (nrow(coords) >= 10) {
+        print("First 10 rows of the file:", quote = FALSE)
+        print(head(coords, n = 10L))
+        print("", quote = FALSE)
+        print("Last 10 rows of the file:", quote = FALSE)
+        print(tail(coords, n = 10L))
+        print("", quote = FALSE)
     }
-  }
-  for (i in 1:nrow(coords)) {
-    if (y[i] < -90 || y[i] > 90) {
-      print("Found an invalid latitude value!", quote = FALSE)
-      print("Row number:", quote = FALSE)
-      print(i+1)
-      print("Value:", quote = FALSE)
-      print(y[i])
-      print("", quote = FALSE)
-      return("Please correct the invalid value(s).")
+    x <- coords$longitude
+    y <- coords$latitude
+    for (i in 1:nrow(coords)) {
+        if (x[i] < 0 || x[i] > 360) {
+            print("Found an invalid longitude value!", quote = FALSE)
+            print("Row number:", quote = FALSE)
+            print(i + 1)
+            print("Value:", quote = FALSE)
+            print(x[i])
+            print("", quote = FALSE)
+            return("Please correct the invalid value(s).")
+        }
     }
-  }
-  
-  print("Original longitude limits:", quote = FALSE)
-  print(range(coords$longitude))
-  print("", quote = FALSE)
-  print("Latitude limits:", quote = FALSE)
-  print(range(coords$latitude))
-  print("", quote = FALSE)
-
-  # Creates a vector with the longitudes in the range [-180, +180]
-  lon180 <- c() 
-  for (i in 1:nrow(coords)) {
-      if (x[i] > 180) {
-        lon180[i] = round(x[i] - 360, digits = 2)
-      } else {
-        lon180[i] = x[i]
-      }
-  }
-  
-  print("New longitude limits:", quote = FALSE)
-  print(range(lon180))
-  print("", quote = FALSE)
-  
-  # Creates a data.frame with the new coordinates and a .txt file (the input of get_country or get_marine)
-  coords180 <- data.frame(longitude = lon180, latitude = coords$latitude)
-  write.table(coords180,file = "coords2getf.txt", row.names = FALSE, col.names = TRUE, sep = "\t", quote = FALSE)
-  print("A file with the longitudes in the range [-180, +180], < coords2getf.txt >, as been created.", quote = FALSE)
-  print("You can run < get_country() > or < get_marine() > now.", quote = FALSE)
-  print("", quote = FALSE)
+    for (i in 1:nrow(coords)) {
+        if (y[i] < -90 || y[i] > 90) {
+            print("Found an invalid latitude value!", quote = FALSE)
+            print("Row number:", quote = FALSE)
+            print(i + 1)
+            print("Value:", quote = FALSE)
+            print(y[i])
+            print("", quote = FALSE)
+            return("Please correct the invalid value(s).")
+        }
+    }
+    print("Original longitude limits:", quote = FALSE)
+    print(range(coords$longitude))
+    print("", quote = FALSE)
+    print("Latitude limits:", quote = FALSE)
+    print(range(coords$latitude))
+    print("", quote = FALSE)
+    lon180 <- c()
+    for (i in 1:nrow(coords)) {
+        if (x[i] > 180) {
+            lon180[i] <- round(x[i] - 360, digits = 2)
+        } else {
+            lon180[i] <- x[i]
+        }
+    }
+    print("New longitude limits:", quote = FALSE)
+    print(range(lon180))
+    print("", quote = FALSE)
+    coords180 <- data.frame(longitude = lon180, latitude = coords$latitude)
+    write.table(coords180, file = "coords2getf.txt", row.names = FALSE,
+      col.names = TRUE, sep = "\t", quote = FALSE)
+    print("A file with the longitudes in the range [-180, +180], < coords2getf.txt >, as been created.", quote = FALSE)
+    print("You can run < get_country() > or < get_marine() > now.", quote = FALSE)
+    print("", quote = FALSE)
 }
